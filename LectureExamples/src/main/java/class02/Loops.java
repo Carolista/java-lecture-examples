9dac9c71550717b19384b66d57e1e288d86ef0a0
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-09 14:49:55 -0600

    Added TODOs for class 2 loops examples

---
+++ LectureExamples/src/main/java/class02/Loops.java
@@ -0,0 +1,25 @@
+package class02;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        // TODO: Construct a basic for loop to print the numbers 1-10
+
+
+        // TODO: Construct a for-each loop to print each element of the words array below
+        String[] words = {"I", "am", "learning", "Java"};
+
+
+        // TODO: Construct a while loop to print each number in the nums array below as long as the value is below 100
+        int[] numbers = {2, 14, 33, 57, 61, 89, 94, 106, 123, 176};
+
+
+        // TODO: Construct a do-while loop to ask the user for movie titles until they enter "QUIT"
+        // HINT: use .nextLine() to handle multi-word strings
+        // Unless quitting, add each title to the collection
+        // Print the collection after the loop closes
+
+    }
+
+}
