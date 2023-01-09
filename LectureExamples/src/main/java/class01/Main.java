c69df2e4428cdeebe18029d85356e87175f89e16
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-09 12:41:22 -0600

    created TODOs for class 1 lecture examples

---
+++ LectureExamples/src/main/java/class01/Main.java
@@ -0,0 +1,39 @@
+package class01;
+
+// TODO: import Scanner from java.util
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO: Use System.out to print to the console
+
+        // TODO: Declare and initialize several variables and print them to the console
+
+        // TODO: Declare and initialize a Scanner object using System.in
+
+        // TODO: Get user input
+        // Precede your input with output to the user so they know what you need
+
+        // Receive the input and store it in a variable
+
+        // Do something with the stored user input
+
+        // Always close the Scanner object at the end of the main method
+        // for efficiency and security
+        // TODO: Close Scanner
+
+
+        // TODO: After coding your methods, call them here
+        // Print a string with print method
+
+        // Use both methods together to print the sum of two numbers
+        // Note: String.valueOf(someNum) will convert an int to a string since print() requires it
+
+    }
+
+    // TODO: Code a method to print a string
+
+    // TODO: Code a method to add two integers
+
+}
