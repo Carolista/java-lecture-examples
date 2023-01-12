895b56188fc0f25de836649a875fee34adbf5d6b
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-12 16:45:44 -0600

    added TODOs for class 3 examples

---
+++ LectureExamples/src/main/java/class03/Library.java
@@ -0,0 +1,17 @@
+package class03;
+
+import java.util.ArrayList;
+
+public class Library {
+
+    // TODO: Declare a field, books, as an ArrayList of Book objects
+    // Give it private access
+
+    // Note: no constructor is required to instantiate a library object
+
+    // TODO: Define a public method, addBook()
+
+    // TODO: Define a public method, printAvailableBooks()
+    // It should print only books that aren't checked out
+
+}
