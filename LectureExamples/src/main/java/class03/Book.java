27364e0b5b83a62bb613b22c6061c6b4605ed6f2
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-13 14:13:12 -0600

    adjusted spacing

--- LectureExamples/src/main/java/class03/Book.java
+++ LectureExamples/src/main/java/class03/Book.java
@@ -35,7 +35,6 @@ public class Book {
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
@@ -43,7 +42,6 @@ public class Book {
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
@@ -55,7 +53,6 @@ public class Book {
     public int getNumPages() {
         return numPages;
     }
-
     public void setNumPages(int numPages) {
         this.numPages = numPages;
     }
