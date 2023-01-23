2dc3f2d30284918cf8006ad9170fe4e8120250b6
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-23 16:17:57 -0600

    coded solution for class 4 lecture examples

--- LectureExamples/src/main/java/class04/Book.java
+++ LectureExamples/src/main/java/class04/Book.java
@@ -4,6 +4,13 @@ public class Book {
 
     // FIELDS
 
+    // TODO: Declare a class-level int field that will hold the next available number for book IDs
+    // Initialize it to 1
+    private static int nextIdNum = 1;
+
+    // TODO: Declare a String field for each unique bookId, but do not initialize it
+    // Make sure each book's ID cannot be changed after it is set in the constructor
+    private final String bookId;
     private String title;
     private String author;
     private boolean available = true;
@@ -12,10 +19,12 @@ public class Book {
 
     // CONSTRUCTORS
 
+    // TODO: In the primary constructor, set the bookId (but not using a parameter) using instance method
     public Book(String title, String author, int numPages) {
         this.title = title;
         this.author = author;
         this.numPages = numPages;
+        this.bookId = generateBookId(); // last because other fields must already have values
     }
 
     public Book(String title, String author) {
@@ -24,6 +33,18 @@ public class Book {
 
 
     // GETTERS & SETTERS
+
+    // TODO: Add a getter for the static field nextIdNum that can be called on the class
+    // Use the IntelliJ generator and notice it makes it a static method automatically
+    // Then customize to make it a meaningful sentence and change the return type to String
+    public static String getNextIdNum() {
+        return "\nThe next available number for a book Id will be " + nextIdNum;
+    }
+
+    // TODO: Add getter for bookId (but no setter since it is final)
+    public String getBookId() {
+        return bookId;
+    }
 
     public String getTitle() {
         return title;
@@ -51,7 +72,26 @@ public class Book {
     }
 
 
+    // OVERRIDING SPECIAL METHODS
+
+    // TODO: Write a custom toString() method that nicely formats the book data
+    public String toString() {
+        return "\n" + getTitleAndAuthor() + "\n" + numPages + " pages \nID: " + bookId;
+    }
+
+
     // INSTANCE METHODS
+
+    // TODO: Define an instance method to generate a unique book Id
+    // It should be private because we want it to be called ONLY from the constructor
+    // Use the format (author initials)-(first three letters of title)-(next available number)
+    // All letters should be capitalized
+    // Increment the next ID number for the class to use next time a new Book object is instantiated
+    private String generateBookId() {
+        String id = Utils.getInitials(author) + "-" + title.substring(0,3).toUpperCase() + "-" + nextIdNum;
+        nextIdNum++;
+        return id;
+    }
 
     String getTitleAndAuthor() {
         return title + " by " + author;
--- LectureExamples/src/main/java/class04/Library.java
+++ LectureExamples/src/main/java/class04/Library.java
@@ -4,10 +4,60 @@ import java.util.ArrayList;
 
 public class Library {
 
-    private ArrayList<Book> books = new ArrayList<>();
+    // TODO: Ensure books cannot be overwritten by another object
+    // Also, make books available to package
+    final ArrayList<Book> books = new ArrayList<>();
+
+    // TODO: Write a custom equals() method to compare the contents of books
+    // First include a reference check, null check, class check, and cast (see chapter 5.3.2.2)
+    // Then compare the size of the books lists
+    // Finally, loop through both lists and compare the books on their title & author
+    public boolean equals(Object toBeCompared) {
+
+        // Reference check
+        if (this == toBeCompared) {
+            return true;
+        }
+
+        // Null check
+        if (toBeCompared == null) {
+            return false;
+        }
+
+        // Class check
+        if (getClass() != toBeCompared.getClass()) {
+            return false;
+        }
+
+        // Cast
+        Library otherLibrary = (Library) toBeCompared;
+
+        // Compare size of lists first
+        if (books.size() != otherLibrary.books.size()) {
+            return false;
+        }
+
+        // Custom comparison of contents
+        for (Book book : books) {
+            int index = otherLibrary.findBook(book.getTitle(), book.getAuthor());
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     public void addBook(Book book) {
         books.add(book);
+    }
+
+    public int findBook(String title, String author) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title) && book.getAuthor().equals(author)) {
+                return books.indexOf(book);
+            }
+        }
+        return -1;
     }
 
     public void printAvailableBooks() {
