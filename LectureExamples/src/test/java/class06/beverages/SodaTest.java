ed163e34299a358633fba9c3903d9ee4620a45e1
Carolista <cjones.renaissancewoman@gmail.com>
2023-01-30 17:02:53 -0600

    added SodaTest file with TODOs for class 6 example

---
+++ LectureExamples/src/test/java/class06/beverages/SodaTest.java
@@ -0,0 +1,19 @@
+package class06.beverages;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SodaTest {
+
+    // TODO: Declare two objects of the Soda class (but do not initialize)
+
+
+    // TODO: Define a @Before block to instantiate the two Soda objects declared above
+
+
+    // TODO: Use the test data above to write 3 tests confirming the inherited properties are set
+
+
+}
