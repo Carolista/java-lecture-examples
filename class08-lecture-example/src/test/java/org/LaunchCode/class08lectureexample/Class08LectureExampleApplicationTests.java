7daf9a1a40291ed0eb1ede93a81b56fbf89e058b
Carolista <cjones.renaissancewoman@gmail.com>
2023-02-05 15:57:52 -0600

    added class 8 example Spring project

---
+++ class08-lecture-example/src/test/java/org/LaunchCode/class08lectureexample/Class08LectureExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.LaunchCode.class08lectureexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class08LectureExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
