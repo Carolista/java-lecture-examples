9dfd542f647863fa898d9b55cdace78c4497132b
Carolista <cjones.renaissancewoman@gmail.com>
2023-02-16 16:52:49 -0600

    updated starter code for class 10 examples

--- class10-lecture-example/src/main/java/org/LaunchCode/class10lectureexample/controllers/CollectionController.java
+++ class10-lecture-example/src/main/java/org/LaunchCode/class10lectureexample/controllers/CollectionController.java
@@ -9,46 +9,39 @@ import org.springframework.web.bind.annotation.*;
 public class CollectionController {
 
     // TODO: Convert this to use a template, collection.html
+    // TODO: Pass list, artCollection, to template
     // Corresponds to http://localhost:8080/collection
     @GetMapping("")
-    public String getCollectionPageContent() {
+    public String displayCollectionPage() {
         System.out.println("\n*** Collection page content requested");
         return "<p>We have an extensive collection of fine art.</p>" +
-                "<p>Click <a href='/collection/form'>here</a> to select a style.</p>";
+                "<ul>" +
+                "<li>Girl with a Pearl Earring</li>" +
+                "<li>Mona Lisa</li>" +
+                "<li>The Birth of Venus</li>" +
+                "<li>The Persistence of Memory</li>" +
+                "<li>The Starry Night</li>" +
+                "</ul>";
     }
 
-    // TODO: Convert this to use a template, styles.html
-    // Corresponds to http://localhost:8080/collection/styles?style=impressionism
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "/styles")
-    public String acceptQueryParamFromCollectionRoute(@RequestParam String style) {
-        System.out.println("\n*** Request submitted with style " + style);
-        return "<h3>" + style.toUpperCase() + " COLLECTION</h3>" +
-                "<p>You are now viewing works of art in the " + style + " style.</p>" +
-                "<p>Click <a href='/collection/form'>here</a> to select a different style.</p>";
-    }
-
-    // TODO: Convert this to use a template, artwork.html
-    // Corresponds to http://localhost:8080/collection/1234
-    @GetMapping("/{artworkId}")
-    public String acceptPathParamFromCollectionRoute(@PathVariable String artworkId) {
-        System.out.println("\nGET Request submitted for artworkId " + artworkId);
-        return "<h3>Collection</h3>" +
-                "<p>Here are details of artwork number" + artworkId + ".</p>";
-    }
-
-    // TODO: Convert this to use a template, collection-form.html
-    // Corresponds to http://localhost:8080/collection/form
-    @GetMapping("/form")
-    public String displayStyleForm() {
-        System.out.println("\nStyle form submitted");
+    // TODO: Convert this to use a template, add-art-form.html
+    // Corresponds to http://localhost:8080/collection/add
+    @GetMapping("/add")
+    public String displayAddArtForm() {
+        System.out.println("\n*** GET request submitted for form content");
         return "<html>" +
                 "<body>" +
-                "<form action='/collection/styles' method='POST'>" +
-                "<p>Enter the style of art you'd like to view:</p>" +
-                "<input type='text' name='style' />" +
-                "<button type='submit'>Submit</button>" +
+                "<form action='/collection' method='POST'>" +
+                "<p>Ooh! New art! What is it called?</p>" +
+                "<input type='text' name='artwork' />" +
+                "<button type='submit'>Add to Collection</button>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+
+    // TODO: Create a POST handler for /add
+    // Should add name of artwork to collection list
+    // Then should redirect to /collection
+
 }
--- class10-lecture-example/src/main/java/org/LaunchCode/class10lectureexample/controllers/GalleryController.java
+++ class10-lecture-example/src/main/java/org/LaunchCode/class10lectureexample/controllers/GalleryController.java
@@ -9,9 +9,10 @@ public class GalleryController {
 
     // TODO: Create and use a template called index.html
     // Remember to remove @ResponseBody
+    // Corresponds to http://localhost:8080
     @GetMapping("/")
     @ResponseBody
-    public String getHomePageContent() {
+    public String displayHomePage() {
         System.out.println("\n*** Home page content requested by browser");
         return "<h2>Midtown Art Gallery</h2>" +
                 "<p>Welcome! View our <a href='/collection'>collection</a> of fine art.</p>";
