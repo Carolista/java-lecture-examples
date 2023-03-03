8ec14034f69befa6bd7533750628eb18590e96df
Carolista <cjones.renaissancewoman@gmail.com>
2023-03-02 20:36:43 -0600

    added class 14 lecture example scripts as starters

---
+++ class14-lecture-examples/class14-CRUD-examples-starter.sql
@@ -0,0 +1,55 @@
+# Create the artwork table
+
+
+# Add a record with all values
+
+
+# Add a record with partial values
+
+
+# Add 2 more records at the same time
+
+
+# Add a new column, on_display, to the existing table
+
+
+# Update the fourth record to indicate that it is on display
+
+
+# Update the second record to assign the period and indicate that it is on display
+
+
+# View all records in the table
+
+
+# View only artwork currently on display
+
+
+# Add a few more records for some of the same artists
+
+
+# View titles, artist, and periods of all artwork by van Gogh
+
+
+# Run a similar query as the last but limit it to 1889
+
+
+# View titles and artist if artwork is by either da Vinci or Vermeer
+
+
+# View all records again, but order by period, ascending
+
+
+# Delete "The Bedroom" record (use ID number)
+
+
+
+# comment one line
+-- comment one line
+
+/* 
+  comment
+  multiple
+  lines
+*/
+
---
+++ class14-lecture-examples/class14-JOIN-examples-starter.sql
@@ -0,0 +1,21 @@
+# Create a new table, artist, with an artist_id, name, and style
+# Add AUTO_INCREMENT keyword for artist_id
+
+
+# Add records for van Gogh, da Vinci, Vermeer, and Monet (ids will auto-generate)
+
+
+# Select an inner join on the artwork table using the artists' names 
+
+
+# Run the previous query but eliminate duplicates 
+
+
+# List all artwork titles and artists and add style using a left inner join
+
+
+# List all artwork titles and artists and add full_name and style using a right inner join
+
+
+# List all records using a full outer join of both tables
+
