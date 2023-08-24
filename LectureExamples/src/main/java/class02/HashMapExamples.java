d6f15850599d6ca63becc9d7e977b648d359d5eb
Carolista <cjones.renaissancewoman@gmail.com>
2023-08-24 13:46:31 -0500

    updated TODOs for class 2 with LOTR theme

---
+++ LectureExamples/src/main/java/class02/HashMapExamples.java
@@ -0,0 +1,36 @@
+package class02;
+
+
+public class HashMapExamples {
+
+    public static void main(String[] args) {
+
+        /* CREATING A HASHMAP AND ADDING KEY/VALUE PAIRS */
+
+        // TODO: Declare an empty HashMap, add key/value pairs, and print the map
+
+        // Create a map, itemsToPack, that can hold String keys and Integer values
+
+        // Add 4 key/value pairs to it representing the elves and their ages
+
+        // Print the map
+
+
+        /* HASHMAP METHODS */
+
+        // TODO: Practice using different methods of the HashMap class
+
+        // Print the size
+
+        // Print the entry set
+
+        // Print just the key set
+
+        // Print just the values
+
+        // Check if it contains a certain key and print result
+
+        // Check if it contains a certain value and print result
+
+    }
+}
