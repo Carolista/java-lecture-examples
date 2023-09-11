3b3a3cb24205cc68e47efe1dba8cb96c1a159c88
Carolista <cjones.renaissancewoman@gmail.com>
2023-09-10 22:24:49 -0400

    updated import statements

--- LectureExamples/src/test/java/class05/LibraryTest.java
+++ LectureExamples/src/test/java/class05/LibraryTest.java
@@ -1,7 +1,7 @@
 package class05;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
