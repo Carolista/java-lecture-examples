d7cdd53d3e9733260a38a050c76e216dfa2a3096
Caroline R. Jones <55961845+Carolista@users.noreply.github.com>
2023-10-16 02:04:56 -0500

    Update class14-CRUD-examples.sql

--- class14-lecture-examples/class14-CRUD-examples.sql
+++ class14-lecture-examples/class14-CRUD-examples.sql
@@ -1,55 +1,47 @@
-# Create the artwork table
+-- Create the artwork table
 
 
-# Add a record with all values
+-- Add a record with all values
 
 
-# Add a record with partial values
+-- Add a record with partial values
 
 
-# Add 2 more records at the same time
+-- Add 2 more records at the same time
 
 
-# Add a new column, on_display, to the existing table
+-- Add a new column, on_display, to the existing table
 
 
-# Update the fourth record to indicate that it is on display
+-- Update the third record to indicate that it is on display
 
 
-# Update the second record to assign the period and indicate that it is on display
+-- Update the second record to assign the style and indicate that it is on display
 
 
-# View all records in the table
+-- View all records in the table
 
 
-# View only artwork currently on display
+-- View only artwork currently on display
 
 
-# Add a few more records for some of the same artists
+-- Add a few more records
 
 
-# View titles, artist, and periods of all artwork by van Gogh
+-- View titles, artist, and styles of all post-impressionist artwork
 
 
-# Run a similar query as the last but limit it to 1889
+-- Run a similar query as the last but only those NOT on display
 
 
-# View titles and artist if artwork is by either da Vinci or Vermeer
+-- View titles and artist if artwork is by either da Vinci or Vermeer
 
 
-# View all records again, but order by period, ascending
+-- View all records again, but order by style, ascending
 
 
-# Delete "The Bedroom" record (use ID number)
+-- Delete "The Church at Auvers" record (use ID number)
 
 
-
-# comment one line
--- comment one line
-
-/* 
-  comment
-  multiple
-  lines
-*/
+-- View all records
 
