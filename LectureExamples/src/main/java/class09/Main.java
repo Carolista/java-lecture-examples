2fad6eb1e2d5d9a8914f661cb11da15abff7554c
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-16 02:06:20 -0400

    updated various files

--- LectureExamples/src/main/java/class09/Main.java
+++ LectureExamples/src/main/java/class09/Main.java
@@ -12,23 +12,48 @@ public class Main {
         System.out.println("The square root of 16 is " + getSquareRoot(16));
         System.out.println("The square root of -5 is " + getSquareRoot(-5));
 
+        System.out.println("The code continues!");
+
+
         // TODO: Print each word in the array, but throw a custom exception if a word has no vowels (aeiou)
         // The custom exception should take the word as an argument, not a message
         // Print the stacktrace if the error is caught
         String[] storyTypes = new String[] {"epic", "fable", "myth", "folktale"};
+        for (String storyType : storyTypes) {
+            try {
+                if (!hasAVowel(storyType)) {
+                    throw new NoVowelException(storyType);
+                } else {
+                    System.out.println(storyType);
+                }
+            } catch (NoVowelException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
     // TODO: Modify the method below to handle the ArrayIndexOutOfBoundsException
     // Print the default message only
     public static void printElement(String[] strArray, int idx) {
-        System.out.println(strArray[idx]);
+        try {
+            System.out.println(strArray[idx]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     // TODO: Modify the method below to throw an ArithmeticException if n is less than 0
     // Print the stacktrace if the error is caught
     public static double getSquareRoot(int n) {
-        return Math.sqrt(n);
+        try {
+            if (n < 0) {
+                throw new ArithmeticException("WARNING: Cannot take square root of negative number " + n);
+            }
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+        return Math.sqrt(Math.abs(n));
     }
 
     // TODO: Introduce a logic error in the method below and use debugging tools to find it
---
+++ LectureExamples/src/main/java/class09/NoVowelException.java
@@ -0,0 +1,8 @@
+package class09;
+
+public class NoVowelException extends Exception {
+
+    public NoVowelException(String word) {
+        super("WARNING: The word '" + word + "' has no standard vowels.");
+    }
+}
