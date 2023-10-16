44480220794cea2fd973e396ae2fbdcea132fbf1
Caroline R. Jones <55961845+Carolista@users.noreply.github.com>
2023-10-16 02:17:19 -0500

    Update class14-JOIN-examples.sql

--- class14-lecture-examples/class14-JOIN-examples.sql
+++ class14-lecture-examples/class14-JOIN-examples.sql
@@ -1,21 +1,20 @@
-# Create a new table, artist, with an artist_id, name, and style
-# Add AUTO_INCREMENT keyword for artist_id
+-- Create a new table, artist, with an artist_id, name, and style
+-- Add AUTO_INCREMENT keyword for artist_id
 
 
-# Add records for van Gogh, da Vinci, Vermeer, and Monet (ids will auto-generate)
+-- Add records for van Gogh, da Vinci, Vermeer, Dalí and Monet (ids will auto-generate)
 
 
-# Select an inner join on the artwork table using the artists' names 
+-- Select an inner join on the artwork table using the artists' names 
 
 
-# Run the previous query but eliminate duplicates 
+-- Run the previous query but eliminate duplicates 
 
 
-# List all artwork titles and artists and add style using a left inner join
+-- List all artwork titles and artists and add nationalities from artist table using a left inner join
 
 
-# List all artwork titles and artists and add full_name and style using a right inner join
+-- List all artwork titles and styles and add full_name and nationality from artist table using a right inner join
 
 
-# List all records using a full outer join of both tables
-
+-- List all records using a full outer join of both tables
