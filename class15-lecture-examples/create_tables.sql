9eb01224820fe85e82e4398fa25c8ffef68d4201
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-19 23:13:33 -0400

    updated starter code for class 15 examples

--- class15-lecture-examples/create_tables.sql
+++ class15-lecture-examples/create_tables.sql
@@ -1,49 +1,44 @@
--- CREATE STYLE TABLE 
--- TODO: Run query
+/* CREATE TABLES & IMPORT DATA FROM CSV FILES */
+
+-- TODO: Run query to create style table
 -- TODO: Import records from styles.csv
-CREATE TABLE style(
+CREATE TABLE style (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     style VARCHAR(50)
 );
 
--- CREATE ARTIST TABLE
--- TODO: Run query
+-- TODO: Run query to create artist table
 -- TODO: Import records from artists.csv
-CREATE TABLE artist(
+CREATE TABLE artist (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     country VARCHAR(20)
 );
 
--- CREATE EXHIBIT TABLE
--- TODO: Run query
-CREATE TABLE exhibit(
+-- TODO: Run query to create exhibit table
+CREATE TABLE exhibit (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(80)
 );
 
--- CREATE ARTWORK TABLE 
 -- TODO: Designate foreign keys for artist, style, and exhibit
--- TODO: Run query
+-- TODO: Run query to create artwork table
 -- TODO: Import records from artworks.csv
-CREATE TABLE artwork(
+CREATE TABLE artwork (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(50),
     details_id INT,
     artist_id INT,
     style_id INT,
     exhibit_id INT,
-    on_display BOOLEAN,
-    FOREIGN KEY (artist_id) REFERENCES artist(id),
-    FOREIGN KEY (style_id) REFERENCES style(id),
-    FOREIGN KEY (exhibit_id) REFERENCES exhibit(id)
+    on_display BOOLEAN
+	-- Add foreign keys here!
 );
 
--- CREATE ARTWORK_DETAILS TABLE
--- TODO: Run query
+-- TODO: Run query to create artwork_details table
 -- TODO: Import records from details.csv
-CREATE TABLE artwork_details(
+CREATE TABLE artwork_details (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     year_created VARCHAR(20),
     media VARCHAR(80)
--- class15-lecture-examples/write_queries.sql
+++ class15-lecture-examples/write_queries.sql
@@ -1,15 +1,19 @@
--- Write a query to display the first and last names of artists 
+/* PERFORM CRUD OPS ON DATA */
+
+-- TODO: Write a query to display the first and last names of artists 
 -- who have artwork in the Impressionist style
--- Remove duplicates, if any
+-- Return without duplicates, if any
 
 
--- Write a query to display the title, artist, and style of each artwork created in 1939
+-- TODO: Write a query to display the title, artist, and style of each artwork created in 1939
 
-
-/* Write queries to handle table updates when putting works on exhibit */
    
--- Create exhibit record 
+-- TODO: Create exhibit record 
 
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+/* TODO: Write a script to handle table updates when putting works on exhibit */
 
 -- Store the new exhibit_id in a variable, @exhibit
 
@@ -20,13 +24,16 @@
 -- Set the exhibit_id for all artworks with the artist_id that is @artist
 
 
--- Change the @artist to another artist and re-run the addition of the exhibit_id for those artworks
-
--- Get total number of artworks assigned to @exhibit and return with exhibit title
-
-
 -- Put all artworks on display for the exhibit (set on_display to true)
 
 
--- Write a query to return the exhibit title, artwork title, artist, year_created, and style of each artwork on display
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- TODO: Change the @artist to another artist and re-run the addition of the exhibit_id for those artworks
+
+
+-- TODO: Get total number of artworks assigned to @exhibit and return with exhibit title
+
+
+-- TODO: Write a query to return the exhibit title, artwork title, artist, year_created, and style of each artwork on display
 -- Order by last name
