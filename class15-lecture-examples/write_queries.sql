db8be0fd31a9a7714109c56cda124fe975d334b8
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-19 23:18:50 -0400

    updated solution for class 15 examples

--- class15-lecture-examples/write_queries.sql
+++ class15-lecture-examples/write_queries.sql
@@ -1,24 +1,28 @@
--- Write a query to display the first and last names of artists 
+/* PERFORM CRUD OPS ON DATA */
+
+-- TODO: Write a query to display the first and last names of artists 
 -- who have artwork in the Impressionist style
--- Remove duplicates, if any
+-- Return without duplicates, if any
 SELECT DISTINCT first_name, last_name
 FROM artist
 INNER JOIN artwork ON artist.id = artwork.artist_id
 WHERE artwork.style_id = (SELECT id FROM style WHERE style.style = "Impressionism");
 
--- Write a query to display the title, artist, and style of each artwork created in 1939
+-- TODO: Write a query to display the title, artist, and style of each artwork created in 1939
 SELECT artwork.title, artist.first_name, artist.last_name, style.style
 FROM artwork
 INNER JOIN artist ON artwork.artist_id = artist.id
 INNER JOIN style ON artwork.style_id = style.id
 INNER JOIN artwork_details ON artwork.details_id = artwork_details.id
 WHERE artwork_details.year_created = "1939";
-
-/* Write queries to handle table updates when putting works on exhibit */
    
--- Create exhibit record 
+-- TODO: Create exhibit record 
 INSERT INTO exhibit(title)
 VALUES("Renaissance and Baroque Masterpieces");
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+/* TODO: Write a script to handle table updates when putting works on exhibit */
 
 -- Store the new exhibit_id in a variable, @exhibit
 SET @exhibit=1;
@@ -31,19 +35,21 @@ UPDATE artwork
 SET exhibit_id=@exhibit
 WHERE artist_id = @artist;
 
--- Change the @artist to another artist and re-run the addition of the exhibit_id for those artworks
-
--- Get total number of artworks assigned to @exhibit and return with exhibit title
-SELECT exhibit.title, COUNT(*) 
-FROM artwork INNER JOIN exhibit ON artwork.exhibit_id = exhibit.id
-WHERE exhibit_id=@exhibit;
-
 -- Put all artworks on display for the exhibit (set on_display to true)
 UPDATE artwork
 SET on_display=1
 WHERE exhibit_id = @exhibit;
 
--- Write a query to return the exhibit title, artwork title, artist, year_created, and style of each artwork on display
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+-- TODO: Change the @artist to another artist and re-run the addition of the exhibit_id for those artworks
+
+-- TODO: Get total number of artworks assigned to @exhibit and return with exhibit title
+SELECT exhibit.title, COUNT(*) 
+FROM artwork INNER JOIN exhibit ON artwork.exhibit_id = exhibit.id
+WHERE exhibit_id=@exhibit;
+
+-- TODO: Write a query to return the exhibit title, artwork title, artist, year_created, and style of each artwork on display
 -- Order by last name
 SELECT exhibit.title, artwork.title, artist.first_name, artist.last_name, artwork_details.year_created, style.style
 FROM artwork
