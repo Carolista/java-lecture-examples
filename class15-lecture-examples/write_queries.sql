3e0e8a2d2ab4b8f7fb2917a34b0f39e984a39259
Carolista <cjones.renaissancewoman@gmail.com>
2023-10-20 00:16:09 -0400

    updated starter code for class 15 examples again

--- class15-lecture-examples/write_queries.sql
+++ class15-lecture-examples/write_queries.sql
@@ -8,32 +8,38 @@
 -- TODO: Write a query to display the title, artist, and style of each artwork created in 1939
 
    
--- TODO: Create exhibit record 
+-- TODO: Create an exhibit record 
 
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 /* TODO: Write a script to handle table updates when putting works on exhibit */
 
--- Store the new exhibit_id in a variable, @exhibit
+-- Create a variable for the exhibit id
 
+-- Create a variable for the artist's last name
 
 -- Look up an artist_id by last_name and store it in a variable, @artist
 
 
 -- Set the exhibit_id for all artworks with the artist_id that is @artist
-
-
--- Put all artworks on display for the exhibit (set on_display to true)
+-- Also set on_display to true
 
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
--- TODO: Change the @artist to another artist and re-run the addition of the exhibit_id for those artworks
+-- TODO: Change the @artist to another artist and re-run script for those artworks
+
+-- TODO: Create another exhibit record, then run the script above for the new exhibit with 2 more artists
 
 
--- TODO: Get total number of artworks assigned to @exhibit and return with exhibit title
+-- TODO: Get total number of artworks assigned to a exhibit and return along with exhibit title
+
+
+-- TODO: Run it again for the second exhibit id
+
+-- TODO: Write a query to get the totals for all exhibits
 
 
 -- TODO: Write a query to return the exhibit title, artwork title, artist, year_created, and style of each artwork on display
--- Order by last name
+-- Order first by exhibit title and then by last name
